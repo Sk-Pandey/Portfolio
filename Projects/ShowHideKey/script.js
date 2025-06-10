c85cc3dd6cf45ed32250f5273b5579ec968f2247
Shashikant Pandey <pandeyshashikant1434@gmail.com>
2025-06-10 15:03:42 +0530

    five commit

--- Projects/ShowHideKey/script.js
+++ Projects/ShowHideKey/script.js
@@ -4,9 +4,9 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
   if (input.type == "text") {
     input.type = "password";
-    btn.innerHTML = `<i class="material-icons h-[24px] w-[24px]">visibility_off</i>`;
+    btn.innerHTML = `<img class="h-[24px] w-[24px]" src="https://img.icons8.com/?size=100&id=85137&format=png&color=000000">`;
   } else {
     input.type = "text";
-    btn.innerHTML = `<i class="material-icons h-[24px] w-[24px]">visibility_on</i>`;
+    btn.innerHTML = `<img class="h-[24px] w-[24px]" src="https://img.icons8.com/?size=100&id=60022&format=png&color=000000">`;
   }
 });
