9c19f57b943d59dd50fac9e50caaf75c6aa25c8a
Shashikant Pandey <pandeyshashikant1434@gmail.com>
2025-06-27 11:31:12 +0530

    new

---
+++ Projects/Calculator/script.js
@@ -0,0 +1,52 @@
+let result = document.getElementById("result");
+let oprators = document.querySelectorAll(".op");
+let digit = document.querySelectorAll(".digit");
+let Button = document.querySelectorAll(".btn");
+
+//disabling each oprator initially
+let op = Array.from(oprators);
+op.forEach((opr) => {
+  opr.disabled = true;
+});
+
+let btns = Array.from(Button);
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (result.innerText == "0") {
+      op.forEach((opr) => {
+        opr.disabled = true;
+      });
+      result.innerText = "";
+    }
+    if (["%", "/", "*", "-", "+"].includes(e.target.innerText)) {
+      result.innerText = result.innerHTML + e.target.innerText;
+      op.forEach((opr) => {
+        opr.disabled = true;
+      });
+    } else if (e.target.innerText == "AC") {
+      result.innerText = "0";
+      op.forEach((opr) => {
+        opr.disabled = true;
+      });
+    } else if (e.target.innerText == "DEL") {
+      result.innerText = result.innerText.slice(0, -1);
+      if (result.innerText == "") {
+        result.innerText = "0";
+      }
+    } else {
+      result.innerText = result.innerHTML + e.target.innerText;
+      op.forEach((opr) => {
+        opr.disabled = false;
+      });
+    }
+  });
+});
+
+let Equal = document.getElementById("equal");
+
+Equal.addEventListener("click", () => {
+  if (["%", "/", "*", "-", "+", "."].includes(result.innerText.slice(-1))) {
+    result.innerText = result.innerText.slice(0, -1);
+  }
+  result.innerText = eval(result.innerText);
+});
